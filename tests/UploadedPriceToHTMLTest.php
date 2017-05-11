e61f64832be835798883d346de709867632d9c94
Maxim Matyushin <matjushin.m@gmail.com>
2017-05-11 18:46:56 +0300

    General class PHPUnit test

---
+++ tests/UploadedPriceToHTMLTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: makc
+ * Date: 11.05.17
+ * Time: 18:28
+ */
+
+namespace MakikaM\PriceToHTML;
+
+use PHPUnit\Framework\TestCase;
+
+class UploadedPriceToHTMLTest extends TestCase
+{
+    
+}
