ab3a8a25f6a284782581df769ae8cfcdda447bf2
MakikaM <matjushin.m@gmail.com>
2017-05-20 13:55:43 +0300

    Added generated pricelist HTML page unit test. And price-assert-file.html in tests/html

--- tests/PricesArrayToHTMLFileTest.php
+++ tests/PricesArrayToHTMLFileTest.php
@@ -14,8 +14,8 @@ class PricesArrayToHTMLFileTest extends TestCase
     {
         $this->_HTMLDir = __DIR__ . '/html/';
         $this->_arPricesExample = [
-            0 => ['ZIP WAX Шампунь 750 мл', 'П00035', '204.09'],
-            1 => ['ЯЩИК, ПОДСТАКАННИК КОНСОЛИ', '96615503', '154.09']
+            0 => ['ZIP WAX Shampoo 750 ml', 'P00035', '204.09'],
+            1 => ['BOX, Some stuff', '96615503', '154.09']
         ];
     }
 
@@ -108,19 +108,29 @@ class PricesArrayToHTMLFileTest extends TestCase
     }
 
 
-    public function testOutputHTMLFileExists()
+    public function testOutputHTMLFileExistAndContentCorrect()
     {
-
+        $testHtmlOutFile = 'test-html-output.html';
 
         $pth = new PricesArrayToHTMLFile(
             $this->_HTMLDir . 'price-test-header.html',
             $this->_HTMLDir . 'price-test-footer.html',
-            $this->_HTMLDir . 'test-html-output.html',
+            $this->_HTMLDir . $testHtmlOutFile,
             $this->_arPricesExample,
             "<tr><td>%1 - %0</td><td>%2</td></tr>\n");
 
-        $this->assertFileExists($this->_HTMLDir.'test-html-output.html');
+        $this->assertFileExists($this->_HTMLDir . $testHtmlOutFile);
 
-        //TODO Сделать проверку на правильное наполнение файла
+        $expectedDom = new \DomDocument();
+        $expectedDom->loadHTMLFile($this->_HTMLDir . 'price-assert-file.html');
+        $expectedDom->preserveWhiteSpace = false;
+
+        $actualDom = new \DomDocument();
+        $actualDom->loadHTMLFile($this->_HTMLDir . $testHtmlOutFile);
+        $actualDom->preserveWhiteSpace = false;
+
+        $this->assertEquals($expectedDom->saveHTML(), $actualDom->saveHTML());
     }
+
+
 }
